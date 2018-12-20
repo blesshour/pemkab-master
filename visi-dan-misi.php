3bd44ad502752deb72e729ca9f7a0c891e80eabf
blesscious <45728403+blesscious@users.noreply.github.com>
2018-12-21 01:36:37 +0800

    Add files via upload

---
+++ visi-dan-misi.php
@@ -0,0 +1,196 @@
+<?=$this->layout('index');?>
+
+ 
+ <!-- HEADER VISI DAN MISI -->
+ <section class="header-selayang" id="header-visi-dan-misi">
+   <div class="jumbotron" style="background-image: url(<?=$this->asset('/images/gallery/gallery7.jpg')?>)">
+    <div class="container wow fadeIn">
+     <h1>Visi dan Misi</h1>
+    </div>
+   </div>
+ </section>
+ <!-- AKHIR HEADER VISI DAN MISI -->
+
+ <div class="container">
+
+  <!-- VISI -->
+  <section class="visi" id="visi">
+   <div class="jumbotron">
+    <div class="container text-center wow fadeInDown">
+      <p class="lead">VISI</p>
+      <h1>"Minahasa Maju Dalam Ekonomi dan Budaya, Berdaulat, Adil, dan Sejahtera"</h1>
+    </div>
+      </div>
+  </section>
+  <!-- AKHIR VISI -->
+
+  <!-- MISI -->
+
+
+<section id='misi' class="misi wow fadeInDown">
+	<div class="container text-center">
+      <p class="lead">MISI</p>
+      
+    </div>
+<div>
+   <div class="container">
+
+            <div class="row">
+                <div class="misi">
+                    <div class="col-md-6 col-xs-12 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                        <div class="misi-wrap">
+                            
+                                <div class="logo">
+                                    <i class="fa fa-users"></i>
+                                </div>
+                                <p>Meningkatkan Pembangunan Sumber Daya Manusia yang Berbudaya dan Berdaya Saing.</p>
+                            
+                        </div>
+                    </div>
+                    <!--/.col-sm-4-->
+                    <div class="col-md-6 col-xs-12 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                        <div class="misi-wrap">
+                            
+                                <div class="logo">
+                                    <i class="fa fa-money"></i>
+                                </div>
+                                <p>Mewujudkan Kemandirian Ekonomi dengan mendorong Sektor Pertanian, Perikanan, dan Pariwisata.</p>
+                            
+                        </div>
+                    </div>
+                    <!--/.col-sm-4-->
+                    <div class="col-md-6 col-xs-12 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                        <div class="misi-wrap">
+                            
+                                <div class="logo">
+                                    <i class="fa fa-building"></i>
+                                </div>
+                                <p>Mewujudkan Pengembangan Kewilayahan dengan Prinsip Pembangunan Berkelanjutan.</p>
+                           
+                        </div>
+                    </div>
+                    <!--/.col-sm-4-->
+
+                    <div class="col-md-6 col-xs-12 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                        <div class="misi-wrap">
+                            
+                                <div class="logo">
+                                    <i class="fa fa-child"></i>
+                                </div>
+                                <p>Meningkatkan Pemerataan Kesejahteraan Masyarakat yang Berkeadilan.</p>
+                           
+                        </div>
+                    </div>
+                    <!--/.col-sm-4-->
+
+                    <div class="col-md-6 col-xs-12 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                        <div class="misi-wrap">
+                            
+                                <div class="logo">
+                                    <i class="fa fa-sitemap"></i>
+                                </div>
+                                <p>Memantapkan Managemen Birokrasi yang Profesional melalui Tata Kelola Pemerintahan yang baik.</p>
+                           
+                        </div>
+                    </div>
+                    <!--/.col-sm-4-->
+
+                </div>
+                <!--/.services-->
+            </div>
+            <!--/.row-->
+        </div>
+        <!--/.container-->
+            
+  </section>
+
+          <!-- AKHIR MISI -->
+
+
+<!-- NAWA CITA -->
+ <section class="nawa-cita" id="nawa-cita">
+ 	<div class="container text-center wow fadeInRight">
+      <p class="lead">NAWA CITA RR-RD</p>
+    </div>
+  <div class="row">
+   <div class="col-lg-4 wow fadeInLeft">
+    <img src="<?=$this->asset('/images/ror/ror1.jpg')?>" alt="Gambar RR-RD" alt="Gambar RR-RD" class="img-fluid">
+   </div>
+   <div class="col-lg-8 wow fadeInRight">
+    <ul>
+    <li>Menghadirkan Pemerintah yang melindungi segenap masyarakat Minahasa, memberikan rasa aman dilandasi kepentingan efektif, demokratis dan berbangsa dan bernegara.</li>
+    <li>Membuat Pemerintah tidak absen dalam memvangun tata kelola pemerintahan yang bersih, dan terpercaya.</li>
+    <li>Membangun Minahasa dari pinggiran dengan memperkuat daerah pedesaan Minahasa.</li>
+    <li>Memperkuat kemandirian ekonomi masyarakat Minahasa melalui percepatan pembangunan sektor pertanian, perikanan dan pariwisata.</li>
+    <li>Meningkatan kualitas hidup masyarakat Kabupaten Minahasa yang sehat, cerdas, terdidik dengan karakter kepribadian yang berdaya saing.</li>
+    <li>Meningkatkan produktifitas rakyat sehingga Minahasa bisa maju dan sejahtera.</li>
+    <li>Membentuk karakter masyarakat Minahasa, dengan memperkuat semangat Mapalus dan Sitou Timou Tou.</li>
+    <li>Memperteguh kebhinekaan dan memperkuat restorasi sosial dengan menciptakan ruang dialog antar warga.</li>
+    <li>Memastikan terselenggaranya pembangunan berkelanjutan yang berwawasan lingkungan.</li>
+    </ul>
+   </div>
+ </section>
+ <!-- AKHIR NAWA CITA -->
+
+ <!-- 22 KEGIATAN UNGGULAN -->
+ <section class="kegiatan-unggulan" id="kegiatan-unggulan">
+ 	<div class="container text-center wow fadeInRight">
+      <p class="lead">22 KEGIATAN UNGGULAN</p>
+    </div>
+  <div class="row">
+   <div class="col-lg-4 wow fadeInLeft">
+    <img src="<?=$this->asset('/images/ror/ror2.jpg')?>" alt="Gambar RR-RD" class="img-fluid">
+   </div>
+   <div class="col-lg-8 wow fadeInRight">
+    <h5>Pendidikan</h5>
+    <ul>
+	    <li>1. Pembangunan Sarana Prasarana Pendidikan</li>
+	    <li>2. Memperkuat Kompetensi Guru (a.l bantuan studi)</li>
+	    <li>3. Selain Bebas biaya sekolah, juga bantuan seragam, sepatu dan sas untuk siswa saru kelas 1 SD dan 1 SMP</li>
+    </ul>
+    <h5>Kesehatan</h5>
+    <ul>
+	    <li>4. Meningkatkan Kualitas Pelayanan Seluruh Puskesmas (BPJS Gratis).</li>
+	    <li>5. Pengadaan Satu unit Ambulance di Setiap Kecamatan.</li>
+	    <li>6. Pembangunan Rumah Layak Huni dan peningkatan Kualitas Rumah Tinggal Keluarga Miskin.</li>
+    </ul>
+    <h5>Pembangunan Fasilitas Publik</h5>
+    <ul>
+	    <li>7. Peningkatan infrastrutur Jalan dan Jembatan.</li>
+	    <li>8. Peningkatan Kualitas Pelayanan Publik.</li>
+	    <li>9. Membangun Fasilitas Pariwisata Di sekitar Danau Tondano, Pantai Timur dan Pantai Barat.</li>
+	    <li>10. Pemberian Banruan Usaha Pertanian, Perkebunan dan Perikanan.</li>
+	    <li>11. Peningkatan Sarana Dan Prasarana Olahraga Di Desa-Desa.</li>
+	    <li>12. Pemberian Bantuan Untuk Peningkatan Sarana Peribadatan.</li>
+	    <li>13. Pemekaran Langowan dan Minahasa Barat.</li>
+    </ul>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-lg-4 wow fadeInLeft">
+    <img src="<?=$this->asset('/images/ror/ror3.jpg')?>" alt="Gambar RR-RD" class="img-fluid">
+   </div>
+   <div class="col-lg-8 wow fadeInRight">
+    <h5>Sosial</h5>
+    <ul>
+	    <li>14. Peningkatan Kerjasama Pembangunan Dengan Elemen Masyarakat termasuk pemberian Insentif Khusus untuk Tokoh Agama dan Tokoh Masyarakat.</li>
+	    <li>15. Bantuan Kesehatan Untuk Penduduk Lanjut Usia</li>
+	    <li>16. Bantuan Dana Duka</li>
+    </ul>
+    <h5>Aparatur Pemerintahan</h5>
+    <ul>
+	    <li>17. Mendorong Profesionalutas Pegawai Negeri Sipil/ASN, Peningkatan Kesejahteraan dan Pemberian Bantuan Studi Lanjut Untuk Pegawai Negeri Sipil.</li>
+	    <li>18. Peningkatan Kesehahteraan Aparat Desa dan Penghargaan Bagi Aparatur Desa Berprestasi.</li>
+    </ul>
+    <h5>Kemasyarakatan</h5>
+    <ul>
+	    <li>19. Dialog Terbuka dengan Masyarakat secara periodik.</li>
+	    <li>20. Menjamin Keamanan, Ketertiban, Kenyamanan Hidup dan Berusaha di Seluruh Wilayah Minahasa.</li>
+	    <li>21. Mendorong Penguatan Karakter dan Budaya Minahasa dengan Prinsip MAPALUS dan SITOU TIMOU TUMUO TOU.</li>
+	    <li>22. Pembentukan Unit Reaksi-Respons-Relief Daerah (R3D) Minahasa.</li>
+    </ul>
+   </div>
+   </div>
+ </section>
+ <!-- AKHIR KEGIATAN UNGGULAN -->
+</div>
