542491b7bb27d74fa010320e35cf56fb44edf4d5
blesscious <45728403+blesscious@users.noreply.github.com>
2018-12-20 23:51:02 +0800

    Create components-featured-news.php

---
+++ inc/components-featured-news.php
@@ -0,0 +1,296 @@
+<section class="" id="featured-news">
+   <div class="container">
+        <div class="center fadeInDown">
+            <h2>Info & Berita</h2>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="news2 owl-carousel">
+                    <div class="item post-slide">
+                        <div class="post-img sbs-slide">
+                            <a href="#"><img src="<?=$this->asset('/images/uploads/kue.jpg')?>" alt=""></a>
+                            <div class="cat-badge">
+                               <h4><span class="label label-warning">Kategori</span></h4>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <h3 class="post-title"><a href="#">Latest News Post</a></h3>
+                            <ul class="post-bar list-inline">
+                                <li><i class="fa fa-calendar"></i> June 5, 2016</li>
+                                <li>
+                                    <i class="fa fa-folder"></i>
+                                    <a href="#">Mockup</a>
+                                    <a href="#">Graphics</a>
+                                    <a href="#">Flayers</a>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                    </div>
+
+                    <div class="item post-slide">
+                        <div class="post-img sbs-slide">
+                            <a href="#"><img src="<?=$this->asset('/images/uploads/lomba-1.jpg')?>" alt=""></a>
+                            <div class="cat-badge">
+                               <h4><span class="label label-warning">Kategori</span></h4>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <h3 class="post-title"><a href="#">Latest News Post</a></h3>
+                            <ul class="post-bar list-inline">
+                                <li><i class="fa fa-calendar"></i> June 7, 2016</li>
+                                <li>
+                                    <i class="fa fa-folder"></i>
+                                    <a href="#">Mockup</a>
+                                    <a href="#">Graphics</a>
+                                    <a href="#">Flayers</a>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                    </div>
+                    
+                    <div class="item post-slide">
+                        <div class="post-img sbs-slide">
+                            <a href="#"><img src="<?=$this->asset('/images/uploads/st_(1).jpg')?>" alt=""></a>
+                            <div class="cat-badge">
+                               <h4><span class="label label-warning">Kategori</span></h4>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <h3 class="post-title"><a href="#">Latest News Post</a></h3>
+                            <ul class="post-bar list-inline">
+                                <li><i class="fa fa-calendar"></i> June 5, 2016</li>
+                                <li>
+                                    <i class="fa fa-folder"></i>
+                                    <a href="#">Mockup</a>
+                                    <a href="#">Graphics</a>
+                                    <a href="#">Flayers</a>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                    </div>
+                    
+                    <div class="item post-slide">
+                        <div class="post-img sbs-slide">
+                            <a href="#"><img src="<?=$this->asset('/images/uploads/wwm-2018.jpg')?>" alt=""></a>
+                            <div class="cat-badge">
+                               <h4><span class="label label-warning">Kategori</span></h4>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <h3 class="post-title"><a href="#">Latest News Post</a></h3>
+                            <ul class="post-bar list-inline">
+                                <li><i class="fa fa-calendar"></i> June 5, 2016</li>
+                                <li>
+                                    <i class="fa fa-folder"></i>
+                                    <a href="#">Mockup</a>
+                                    <a href="#">Graphics</a>
+                                    <a href="#">Flayers</a>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="news2 owl-carousel">
+                <div class="item post-slide">
+                        <div class="post-img sbs-slide">
+                            <a href="#"><img src="<?=$this->asset('/images/uploads/ror.jpg')?>" alt=""></a>
+                            <div class="cat-badge">
+                               <h4><span class="label label-warning">Kategori</span></h4>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <h3 class="post-title"><a href="#">Latest News Post</a></h3>
+                            <ul class="post-bar list-inline">
+                                <li><i class="fa fa-calendar"></i> June 5, 2016</li>
+                                <li>
+                                    <i class="fa fa-folder"></i>
+                                    <a href="#">Mockup</a>
+                                    <a href="#">Graphics</a>
+                                    <a href="#">Flayers</a>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                    </div>
+
+                    <div class="item post-slide">
+                        <div class="post-img sbs-slide">
+                            <a href="#"><img src="<?=$this->asset('/images/uploads/rd_(2).jpg')?>" alt=""></a>
+                            <div class="cat-badge">
+                               <h4><span class="label label-warning">Kategori</span></h4>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <h3 class="post-title"><a href="#">Latest News Post</a></h3>
+                            <ul class="post-bar list-inline">
+                                <li><i class="fa fa-calendar"></i> June 7, 2016</li>
+                                <li>
+                                    <i class="fa fa-folder"></i>
+                                    <a href="#">Mockup</a>
+                                    <a href="#">Graphics</a>
+                                    <a href="#">Flayers</a>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                    </div>
+                    
+                    <div class="item post-slide">
+                        <div class="post-img sbs-slide">
+                            <a href="#"><img src="<?=$this->asset('/images/uploads/pelantikan.jpg')?>" alt=""></a>
+                            <div class="cat-badge">
+                               <h4><span class="label label-warning">Kategori</span></h4>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <h3 class="post-title"><a href="#">Latest News Post</a></h3>
+                            <ul class="post-bar list-inline">
+                                <li><i class="fa fa-calendar"></i> June 5, 2016</li>
+                                <li>
+                                    <i class="fa fa-folder"></i>
+                                    <a href="#">Mockup</a>
+                                    <a href="#">Graphics</a>
+                                    <a href="#">Flayers</a>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                    </div>
+                    
+                    <div class="item post-slide">
+                        <div class="post-img sbs-slide">
+                            <a href="#"><img src="<?=$this->asset('/images/uploads/lomba-1.jpg')?>" alt=""></a>
+                            <div class="cat-badge">
+                               <h4><span class="label label-warning">Kategori</span></h4>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <h3 class="post-title"><a href="#">Latest News Post</a></h3>
+                            <ul class="post-bar list-inline">
+                                <li><i class="fa fa-calendar"></i> June 5, 2016</li>
+                                <li>
+                                    <i class="fa fa-folder"></i>
+                                    <a href="#">Mockup</a>
+                                    <a href="#">Graphics</a>
+                                    <a href="#">Flayers</a>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+ 
+                <div class="col-md-12">
+                    <div id="news-slider2" class="owl-carousel">
+                        
+                        <div class="post-slide">
+                            <div class="post-img hrslide">
+                                <a href="#"><img src="<?=$this->asset('/images/uploads/mou-bpjs-kesehatan.jpg')?>" alt="" ></a>
+                                <div class="cat-badge">
+                                  <h4><span class="label label-warning">Kategori</span></h4>
+                               </div>
+                            </div>
+                            <div class="post-content">
+                                <h3 class="post-title"><a href="#">Latest News Post</a></h3>
+                                <ul class="post-bar list-inline">
+                                    <li><i class="fa fa-calendar"></i> June 5, 2016</li>
+                                    <li>
+                                        <i class="fa fa-folder"></i>
+                                        <a href="#">Mockup</a>
+                                        <a href="#">Graphics</a>
+                                        <a href="#">Flayers</a>
+                                    </li>
+                                </ul>
+                                
+                            </div>
+                        </div>
+
+                        <div class="post-slide">
+                            <div class="post-img hrslide">
+                                <a href="#"><img src="<?=$this->asset('/images/uploads/naik-bendi-menuju.jpg')?>" alt="" ></a>
+                                <div class="cat-badge">
+                                  <h4> <span class="label label-warning">Kategori</span></h4>
+                               </div>
+                            </div>
+                            <div class="post-content">
+                                <h3 class="post-title"><a href="#">Latest News Post</a></h3>
+                                <ul class="post-bar list-inline">
+                                    <li><i class="fa fa-calendar"></i> June 7, 2016</li>
+                                    <li>
+                                        <i class="fa fa-folder"></i>
+                                        <a href="#">Mockup</a>
+                                        <a href="#">Graphics</a>
+                                        <a href="#">Flayers</a>
+                                    </li>
+                                </ul>
+                                
+                            </div>
+                        </div>
+                        
+                        <div class="post-slide">
+                            <div class="post-img hrslide">
+                                <a href="#"><img src="<?=$this->asset('/images/uploads/1024.jpg')?>" alt="" ></a>
+                                <div class="cat-badge">
+                                  <h4> <span class="label label-warning">Kategori</span></h4>
+                               </div>
+                            </div>
+                            <div class="post-content">
+                                <h3 class="post-title"><a href="#">Latest News Post</a></h3>
+                                <ul class="post-bar list-inline">
+                                    <li><i class="fa fa-calendar"></i> June 5, 2016</li>
+                                    <li>
+                                        <i class="fa fa-folder"></i>
+                                        <a href="#">Mockup</a>
+                                        <a href="#">Graphics</a>
+                                        <a href="#">Flayers</a>
+                                    </li>
+                                </ul>
+                                
+                            </div>
+                        </div>
+                        
+                        <div class="post-slide">
+                            <div class="post-img hrslide">
+                                <a href="#"><img src="<?=$this->asset('/images/uploads/fbimg.jpg')?>" alt="" ></a>
+                                <div class="cat-badge">
+                                  <h4> <span class="label label-warning">Kategori</span></h4>
+                               </div>
+                            </div>
+                            <div class="post-content">
+                                <h3 class="post-title"><a href="#">Latest News Post</a></h3>
+                                <ul class="post-bar list-inline">
+                                    <li><i class="fa fa-calendar"></i> June 5, 2016</li>
+                                    <li>
+                                        <i class="fa fa-folder"></i>
+                                        <a href="#">Mockup</a>
+                                        <a href="#">Graphics</a>
+                                        <a href="#">Flayers</a>
+                                    </li>
+                                </ul>
+                                
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            
+
+        </div>
+        <!-- /.endrow -->
+        <div class="clearfix text-center">
+            <br>
+            <br>
+            <a href="berita.php" class="btn btn-primary">Lihat lebih banyak</a>
+        </div>
+    </div>
+    <!--/.container-->
+</section>
