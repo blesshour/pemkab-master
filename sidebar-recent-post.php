cf3851d26820c499294bed693fe6f225ef9e4d87
blesscious <45728403+blesscious@users.noreply.github.com>
2018-12-21 16:50:48 +0800

    Add files via upload

---
+++ sidebar-recent-post.php
@@ -0,0 +1,51 @@
+<!-- Begin fluid width widget -->
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <h3 class="panel-title">
+            <span class="glyphicon glyphicon-list-alt"></span> Recent Posts
+        </h3>
+    </div>
+    <div class="panel-body">
+        <ul class="media-list">
+            <li class="media">
+                <div class="media-body">
+                    <h4 class="media-heading">
+                                    <a href="#" class="text-info">
+                                        Ultricies Luctus
+                                    </a>
+                                </h4>
+                    <p class="margin-top-10 margin-bottom-20">
+                        Ut sit amet tincidunt risus. Aenean quis tempus tortor. Pellentesque commodo malesuada augue...
+                    </p>
+                </div>
+            </li>
+            <li class="media">
+                <div class="media-body">
+                    <h4 class="media-heading">
+                        <a href="#" class="text-info">
+                            Nulla Vitae
+                        </a>
+                    </h4>
+                    <p class="margin-top-10 margin-bottom-20">
+                        Pellentesque id nulla vitae nisl mollis hendrerit a non nisi. Cras ac velit elit. Fusce nulla odio, iaculis...
+                    </p>
+                </div>
+            </li>
+            <li class="media">
+                <div class="media-body">
+                    <h4 class="media-heading">
+                        <a href="#" class="text-info">
+                            Media Heading
+                        </a>
+                    </h4>
+                    <p class="margin-top-10 margin-bottom-20">
+                        Donec venenatis, orci sit amet tempor dapibus, nisi elit imperdiet ex, nec lobortis ex magna eu felis...
+                    </p>
+                </div>
+            </li>
+        </ul>
+        <a href="#" class="btn btn-default btn-block">More Blog Posts »</a>
+    </div>
+</div>
+<!-- End fluid width widget -->
+</div>
