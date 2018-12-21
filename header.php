6ea63d5433a6039ad6d7d9b18e8eb947affa56ef
blesscious <45728403+blesscious@users.noreply.github.com>
2018-12-21 17:41:20 +0800

    Update header.php

--- header.php
+++ header.php
@@ -12,31 +12,9 @@
             </div>
 
             <div class="collapse navbar-collapse navbar-center">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="index.html">Beranda</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pemerintahan<i class="fa fa-angle-down"></i></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="profil-bupati.html">Profil Bupati</a></li>
-                            <li><a href="profil-wakil-bupati.html">Profil Wakil Bupati</a></li>
-                            <li><a href="kecamatan-dan-kelurahan.html">Kecamatan & Kelurahan</a></li>
-                            <li><a href="perangkat-daerah.html">Perangkat Daerah</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Selayang Pandang<i class="fa fa-angle-down"></i></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="visi-dan-misi.html">Visi & Misi</a></li>
-                            <li><a href="lambang-dan-arti.html">Lambang & Arti</a></li>
-                            <li><a href="sejarah.html">Sejarah</a></li>
-                            <li><a href="peta-dan-topografi.html">Peta & Topografi</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="berita.html">Berita</a></li>
-                    <li><a href="layanan-publik.html">Layanan Publik</a></li>
-                    <li><a href="galeri.html">Galeri</a></li>
-                    <li><a href="hubungi-kami.html">Hubungi Kami</a></li>
-                </ul>
+                <?php
+                    echo $this->menu()->getFrontMenu(WEB_LANG, 'class="nav navbar-nav"', 'class="dropdown"', 'class="dropdown-menu"');
+                ?>
             </div>
         </div>
         <!--/.container-->
