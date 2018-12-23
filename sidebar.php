c6bb35468c1794c51bcb721a75e5e770a0aa63a6
blesscious <45728403+blesscious@users.noreply.github.com>
2018-12-24 00:26:45 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -14,25 +14,6 @@
     </div>                     
 </div><!--/.categories-->
 
-<div class="suara-anda-slider owl-carousel owl-theme">
-    <div class="single-slide">
-        <div class="fadeInRight">
-            <p>dari <strong>Gloria Borger</strong>
-                <small class="text-muted">kategori: Kritikan || Senin, 10 Oktober 2018 </small></p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quos atque eveniet. Vitae distinctio, numquam laboriosam nisi ullam ipsam. Magni, dolorem adipisci illum nostrum incidunt voluptatibus rem accusantium distinctio sit!</p>
-        </div>
-    </div>
-
-    <div class="single-slide">
-        <div class="fadeInRight">
-            <p>dari <strong>Gloria Geger</strong>
-                <small class="text-muted">kategori: Kritikan || Senin, 10 Oktober 2018 </small></p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quos atque eveniet. Vitae distinctio, numquam laboriosam nisi ullam ipsam. Magni, dolorem adipisci illum nostrum incidunt voluptatibus rem accusantium distinctio sit!</p>
-        </div>
-    </div>
-</div>
-
-
         <div class="widget tags">
             <h3><?=$this->e($front_tag);?></h3>
             <ul class="tag-cloud">
