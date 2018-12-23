870f811a13cc3388cd35cba4a49a576caeffe713
blesscious <45728403+blesscious@users.noreply.github.com>
2018-12-23 15:02:19 +0800

    service coding

--- inc/components-welcome-and-service.php
+++ inc/components-welcome-and-service.php
@@ -1,8 +1,8 @@
 <section class="center" id="welcome">
    <h6 class="animation animated-item-1">Selamat Datang di website </h6>
-   <h3 class="animation animated-item-2">Pemerintah Kabupten</h3>
+   <h3 class="animation animated-item-2">Pemerintah Kabupaten</h3>
    <h1 class="animation animated-item-2">MINAHASA</h1>
-   <a class="btn-slide animation animated-item-3" href="#">Visi & Misi</a>
+   <a class="btn-slide animation animated-item-3" href="<?=BASE_URL.'/pages/'.$this->e($pages['visi-dan-misi']);?>">Visi & Misi</a>
 </section>
 
 <section id="service">
@@ -10,12 +10,9 @@
         <div class="row">
             <div class="col-sm-4 item animation animated-item-4 fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
                 <div class="service-wrap">
-                    <a href="">
-                        <div class="icon">
-                            <i class="fa fa-phone"></i>                        
-                        </div>
+                    <a href="#nomor-telepon-penting">
                         <div class="capt">
-                            <h2>Nomor Penting</h2>
+                            <h2><span class="icon"><i class="fa fa-phone"></i></span> Nomor Penting</h2>
                         </div>
                     </a>
                 </div>
@@ -24,11 +21,8 @@
             <div class="col-sm-4 item animation animated-item-4 fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
                 <div class="service-wrap">
                     <a href="">
-                        <div class="icon">
-                            <i class="fa fa-handshake-o"></i>
-                        </div>
                         <div class="capt">
-                            <h2>Layanan Publik</h2>
+                            <h2><span class="icon"><i class="fa fa-handshake-o"></i></span> Layanan Publik</h2>
                         </div>
                     </a>
                 </div>
@@ -36,12 +30,9 @@
             <!--/.col-sm-4-->
             <div class="col-sm-4 item animation animated-item-4 fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
                 <div class="service-wrap">
-                    <a href="">
-                        <div class="icon">
-                            <i class="fa fa-commenting"></i>
-                        </div>
+                    <a href="#suara-anda">
                         <div class="capt">
-                            <h2>Suara Anda</h2>
+                            <h2><span class="icon"><i class="fa fa-commenting"></i></i></span> Suara Anda</h2>
                         </div>
                     </a>
                 </div>
