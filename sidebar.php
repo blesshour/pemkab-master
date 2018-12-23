579818f0f8ef7524a3ca09d9d28a7ad1083bbfdd
pricelangela <45728920+pricelangela@users.noreply.github.com>
2018-12-23 23:56:14 +0800

    profil bupati + wbupati update

--- sidebar.php
+++ sidebar.php
@@ -20,17 +20,17 @@
             <p>dari <strong>Gloria Borger</strong>
                 <small class="text-muted">kategori: Kritikan || Senin, 10 Oktober 2018 </small></p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quos atque eveniet. Vitae distinctio, numquam laboriosam nisi ullam ipsam. Magni, dolorem adipisci illum nostrum incidunt voluptatibus rem accusantium distinctio sit!</p>
-            </div>
         </div>
+    </div>
 
-        <div class="single-slide">
-            <div class="fadeInRight">
-                <p>dari <strong>Gloria Geger</strong>
-                    <small class="text-muted">kategori: Kritikan || Senin, 10 Oktober 2018 </small></p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quos atque eveniet. Vitae distinctio, numquam laboriosam nisi ullam ipsam. Magni, dolorem adipisci illum nostrum incidunt voluptatibus rem accusantium distinctio sit!</p>
-                </div>
-            </div>
+    <div class="single-slide">
+        <div class="fadeInRight">
+            <p>dari <strong>Gloria Geger</strong>
+                <small class="text-muted">kategori: Kritikan || Senin, 10 Oktober 2018 </small></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quos atque eveniet. Vitae distinctio, numquam laboriosam nisi ullam ipsam. Magni, dolorem adipisci illum nostrum incidunt voluptatibus rem accusantium distinctio sit!</p>
         </div>
+    </div>
+</div>
 
 
         <div class="widget tags">
