38ef61e9bac403c1bed6863cd354c62658799f56
blesscious <45728403+blesscious@users.noreply.github.com>
2018-12-24 20:40:17 +0800

    Add files via upload

---
+++ sidebar-ann.php
@@ -0,0 +1,17 @@
+<div class="sidebar-ann owl-carousel owl-theme">
+    <div class="single-slide">
+        <div class="fadeInRight">
+            <p>dari <strong>Gloria Borger</strong>
+            <small class="text-muted">kategori: Kritikan || Senin, 10 Oktober 2018 </small></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quos atque eveniet. Vitae distinctio, numquam laboriosam nisi ullam ipsam. Magni, dolorem adipisci illum nostrum incidunt voluptatibus rem accusantium distinctio sit!</p>
+        </div>
+    </div>
+
+    <div class="single-slide">
+        <div class="fadeInRight">
+            <p>dari <strong>Gloria Geger</strong>
+            <small class="text-muted">kategori: Kritikan || Senin, 10 Oktober 2018 </small></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quos atque eveniet. Vitae distinctio, numquam laboriosam nisi ullam ipsam. Magni, dolorem adipisci illum nostrum incidunt voluptatibus rem accusantium distinctio sit!</p>
+        </div>
+    </div>
+</div>
---
+++ sidebar-banners2.php
@@ -0,0 +1,8 @@
+<div class="small-banner-slider owl-carousel">
+    <div class="item">
+        <img class="img-responsive" src="<?=$this->asset('/images/uploads/bannernatal01.png')?>" alt="">
+    </div>
+    <div class="item">
+        <img class="img-responsive" src="<?=$this->asset('/images/uploads/bannernatal02.png')?>" alt="">
+    </div>
+</div>
