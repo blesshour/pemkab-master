753ca8e2f98df22c41ee6f9e3a44196aa9a516b6
BillyOroh <45728852+BillyOroh@users.noreply.github.com>
2018-12-24 20:45:08 +0800

    Update components-socmed.php

--- inc/components-socmed.php
+++ inc/components-socmed.php
@@ -3,22 +3,22 @@
       <h2>Social Media</h2>
    </div>
 
-   <div class="socmed center wow fadeInDown">
+   <div class="socmed center">
       <ul>
-         <li>
-            <a href="#"><i class="logo fa fa-facebook-square  fa-2x fadeInDown " data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
+         <li class="wow zoomIn" data-wow-duration="1000ms">
+            <a href="#"><i class="logo fa fa-facebook-square  fa-2x" data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
          </li>
-         <li>
-            <a href="#"><i class="logo fa fa-twitter-square fa-2x fadeInDown " data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
+         <li class="wow zoomIn" data-wow-duration="1500ms">
+            <a href="#"><i class="logo fa fa-twitter-square fa-2x" data-wow-duration="1500ms " data-wow-delay="900ms"></i></a>
          </li>
-         <li>
-            <a href="#"><i class="logo fa fa-instagram fa-2x fadeInDown " data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
+         <li class="wow zoomIn" data-wow-duration="2000ms">
+            <a href="#"><i class="logo fa fa-instagram fa-2x" data-wow-duration="2000ms " data-wow-delay="900ms"></i></a>
          </li>
-         <li>
-            <a href="#"><i class="logo fa fa-google-plus fa-2x fadeInDown " data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
+         <li class="wow zoomIn" data-wow-duration="2500ms">
+            <a href="#"><i class="logo fa fa-google-plus fa-2x" data-wow-duration="2500ms " data-wow-delay="900ms"></i></a>
          </li>
-         <li>
-            <a href="#"><i class="logo fa fa-youtube fa-2x fadeInDown" data-wow-duration="1000ms " data-wow-delay="900ms"></i></a>
+         <li class="wow zoomIn" data-wow-duration="3000ms">
+            <a href="#"><i class="logo fa fa-youtube fa-2x" data-wow-duration="3000ms " data-wow-delay="900ms"></i></a>
          </li>
       </ul>
    </div>
