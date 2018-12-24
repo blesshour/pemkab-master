bbecff3641e961711b9c90db9717cb69e6989e4b
pricelangela <45728920+pricelangela@users.noreply.github.com>
2018-12-24 23:57:55 +0800

    Add files via upload

--- sidebar.php
+++ sidebar.php
@@ -1,19 +1,3 @@
-<div class="widget categories">
-    <h3><?=$this->e($front_categories);?></h3>
-    <div class="row">
-        <div class="col-sm-6">
-            <ul class="blog_category">
-                <?php
-                $categorys_side = $this->category()->getAllCategory(WEB_LANG_ID);
-                foreach($categorys_side as $category_side){
-                    ?>
-                    <li><a href="<?=BASE_URL;?>/category/<?=$category_side['seotitle'];?>"><?=$category_side['title'];?></a></li>
-                <?php } ?>
-            </ul>
-        </div>
-    </div>                     
-</div><!--/.categories-->
-
         <div class="widget tags">
             <h3><?=$this->e($front_tag);?></h3>
             <ul class="tag-cloud">
@@ -24,24 +8,26 @@
         <!--sosmed-->
         <div class="sidebar-widgets-wrap clearfix">
         <div class="widget clearfix">
-                <div class="col_one_third nobottommargin">
-                    <a href="#" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
+            <div class="row">
+                <div class="col-xs-4">
+                    <a href="#" class="col-xs-2 nobottommargin" style="margin-right: 10px;">
                         <i class="fa fa-facebook-square fa-2x"></i>
                     </a>
-                    <small style="display: block; margin-top: 3px;"><strong><div class="counter counter-inherit"><span data-from="500" data-to="<?=$this->oauth()->getFacebookCount("http://www.popojicms.org");?>" data-refresh-interval="50" data-speed="2500" data-comma="true"></span></div></strong>Share</small>
+                    <small style="display: block; margin-top: 3px; text-align: center;"><strong><div class="counter counter-inherit col-xs-10"><span data-from="500" data-to="<?=$this->oauth()->getFacebookCount("http://www.popojicms.org");?>" data-refresh-interval="50" data-speed="2500" data-comma="true"></span></div></strong>Share</small>
                 </div>
-                <div class="col_one_third nobottommargin">
-                    <a href="#" class="social-icon si-dark si-colored si-twitter nobottommargin" style="margin-right: 10px;">
+                <div class="col-xs-4">
+                    <a href="#" class="col-xs-2 nobottommargin" style="margin-right: 10px;">
                         <i class="fa fa-twitter-square fa-2x"></i>
                     </a>
-                    <small style="display: block; margin-top: 3px;"><strong><div class="counter counter-inherit"><span data-from="500" data-to="<?=$this->oauth()->getTwitterCount("DwiraSurvivor");?>" data-refresh-interval="50" data-speed="2500" data-comma="true"></span></div></strong>Followers</small>
+                    <small style="display: block; margin-top: 3px; text-align: center;"><strong><div class="counter counter-inherit col-xs-10"><span data-from="500" data-to="<?=$this->oauth()->getTwitterCount("DwiraSurvivor");?>" data-refresh-interval="50" data-speed="2500" data-comma="true"></span></div></strong>Followers</small>
                 </div>
-                <div class="col_one_third nobottommargin col_last">
-                    <a href="#" class="social-icon si-dark si-colored si-youtube nobottommargin" style="margin-right: 10px;">
+                <div class="col-xs-4 col_last">
+                    <a href="#" class="col-xs-2 nobottommargin" style="margin-right: 10px;">
                         <i class="fa fa-youtube fa-2x"></i>
                     </a>
-                    <small style="display: block; margin-top: 3px;"><strong><div class="counter counter-inherit"><span data-from="500" data-to="<?=$this->oauth()->getSubscribeCount();?>" data-refresh-interval="50" data-speed="2500" data-comma="true"></span></div></strong>Subscribe</small>
+                    <small style="display: block; margin-top: 3px; text-align: center;"><strong><div class="counter counter-inherit col-xs-10"><span data-from="500" data-to="<?=$this->oauth()->getSubscribeCount();?>" data-refresh-interval="50" data-speed="2500" data-comma="true"></span></div></strong>Subscribe</small>
                 </div>
             </div>
-            </div>
+        </div>
+        </div>
             <!--sosmed end-->
