26ecb91c9e9bb3582a0f82b38f5593ff7f6b508b
blesscious <45728403+blesscious@users.noreply.github.com>
2018-12-26 06:14:21 +0800

    Delete sidebar-banners2.php

--- inc/sidebar-banners2.php
+++
@@ -1,8 +0,0 @@
-<div class="small-banner-slider owl-carousel">
-    <div class="item">
-        <img class="img-responsive" src="<?=$this->asset('/images/uploads/bannernatal01.png')?>" alt="">
-    </div>
-    <div class="item">
-        <img class="img-responsive" src="<?=$this->asset('/images/uploads/bannernatal02.png')?>" alt="">
-    </div>
-</div>
