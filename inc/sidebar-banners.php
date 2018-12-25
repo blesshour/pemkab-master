6396f89da3499fa526a01f77ce4f08a60c43c35b
blesscious <45728403+blesscious@users.noreply.github.com>
2018-12-26 06:14:27 +0800

    Delete sidebar-banners.php

--- inc/sidebar-banners.php
+++
@@ -1,8 +0,0 @@
-<div class="small-banner-slider owl-carousel">
-    <div class="item">
-        <img class="img-responsive" src="<?=$this->asset('/images/uploads/59.jpg')?>" alt="">
-    </div>
-    <div class="item">
-        <img class="img-responsive" src="<?=$this->asset('/images/uploads/duka.jpg')?>" alt="">
-    </div>
-</div>
