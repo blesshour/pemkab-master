df149f39febead7313f8c953652c66cd39f3709c
blesscious <45728403+blesscious@users.noreply.github.com>
2018-12-26 22:26:58 +0800

    share link fb & layout comments

--- detailpost.php
+++ detailpost.php
@@ -38,32 +38,31 @@ $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
                     <?php if ($post['comment'] == 'Y') { ?>
                     <?php if ($this->post()->getCountComment($post['id_post']) > 0) { ?>
-                    <h1 id="comments_title"><?=$this->post()->getCountComment($post['id_post']);?> <?=$this->e($front_comment);?></h1>
-                    <?php
-                        $com_parent = $this->post()->getCommentByPost($post['id_post'], '3', 'DESC', $this->e($page));
-                        $com_template = array(
-                            'parent_tag_open' => '<div class="media comment_section">',
-                            'parent_tag_close' => '</div>',
-                            'child_tag_open' => '<div class="media comment_section">',
-                            'child_tag_close' => '</div>',
-                            'comment_list' => '
-                                <div class="pull-left post_comments">
-                                    <a href="{$comment_url}"><img src="{$comment_avatar}" class="img-circle" alt="" /></a>
-                                </div>
-                                <div class="media-body post_reply_comments">
-                                    <h3>{$comment_name}</h3>
-                                    <h4>{$comment_datetime}</h4>
-                                    <p>{$comment_content}</p>
-                                </div>
-                            '
-                        );
-                    ?>
-                    <?=$this->post()->generateComment($com_parent, 'DESC', $com_template);?>
-                    <div class="text-center">
-                        <ul class="pagination pagination-lg">
-                            <?=$this->post()->getCommentPaging('3', $post['id_post'], $post['seotitle'], $this->e($page), '1', $this->e($front_paging_prev), $this->e($front_paging_next));?>
-                        </ul><!--/.pagination-->
-                    </div>
+                        <h1 id="comments_title"><?=$this->post()->getCountComment($post['id_post']);?> <?=$this->e($front_comment);?></h1>
+                        <?php
+                            $com_parent = $this->post()->getCommentByPost($post['id_post'], '3', 'DESC', $this->e($page));
+                            $com_template = array(
+                                'parent_tag_open' => '<div class="media comment_section">',
+                                'parent_tag_close' => '</div>',
+                                'child_tag_open' => '<div class="media comment_section">',
+                                'child_tag_close' => '</div>',
+                                'comment_list' => '
+                                    <div class="pull-left post_comments">
+                                        <a href="{$comment_url}"><img src="{$comment_avatar}" class="img-circle" alt="" /></a>
+                                    </div>
+                                    <div class="media-body post_reply_comments">
+                                        <h3>{$comment_name}</h3>
+                                        <h4>{$comment_datetime}</h4>
+                                        <p>{$comment_content}</p>
+                                    </div>
+                                '
+                        );?>
+                        <?=$this->post()->generateComment($com_parent, 'DESC', $com_template);?>
+                        <div class="text-center">
+                            <ul class="pagination pagination-lg">
+                                <?=$this->post()->getCommentPaging('3', $post['id_post'], $post['seotitle'], $this->e($page), '1', $this->e($front_paging_prev), $this->e($front_paging_next));?>
+                            </ul><!--/.pagination-->
+                        </div>
                     <?php } ?>
 
                     <div id="contact-page clearfix">
@@ -76,36 +75,35 @@ $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
                             <input type="hidden" name="id" name="id" value="<?=$post['id_post'];?>" />
                             <input type="hidden" name="seotitle" id="seotitle" value="<?=$post['seotitle'];?>" />
                             <div class="row">
-                                <div class="col-sm-5">
-                                    <div class="form-group">
+                                <div class="form-inline" role="form">
+                                    <div class="col-sm-4 col-xs-12 form-group">
                                         <label><?=$this->e($comment_name);?> *</label>
                                         <input type="text" class="form-control" value="<?=(isset($_POST['name']) ? $_POST['name'] : '');?>" required="required" />
                                     </div>
-                                    <div class="form-group">
+                                    <div class="col-sm-4 col-xs-12 form-group">
                                         <label><?=$this->e($comment_email);?> *</label>
                                         <input type="email" class="form-control" value="<?=(isset($_POST['email']) ? $_POST['email'] : '');?>" required="required" />
                                     </div>
-                                    <div class="form-group">
+                                    <div class="col-sm-4 col-xs-12 form-group">
                                         <label><?=$this->e($comment_website);?></label>
                                         <input type="url" class="form-control" value="<?=(isset($_POST['url']) ? $_POST['url'] : '');?>" />
-                                    </div>                    
-                                </div>
-                                <div class="col-sm-7">                        
-                                    <div class="form-group">
-                                        <label><?=$this->e($comment_text);?> *</label>
-                                        <textarea name="message" id="message" required="required" class="form-control" rows="3"><?=(isset($_POST['comment']) ? $_POST['comment'] : '');?></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="g-recaptcha" data-sitekey="<?=$this->pocore()->call->posetting[21]['value'];?>"></div>
-                                    </div>  
-                                    <div class="form-group">
-                                        <button type="submit" class="btn btn-primary btn-lg" required="required"><?=$this->e($comment_submit);?></button>
                                     </div>
                                 </div>
+                                                                    
+                                <div class="col-sm-12 form-group">
+                                    <label><?=$this->e($comment_text);?> *</label>
+                                    <textarea name="message" id="message" required="required" class="form-control" rows="3"><?=(isset($_POST['comment']) ? $_POST['comment'] : '');?></textarea>
+                                </div>
+                                <div class="col-sm-6 form-group">
+                                    <div class="g-recaptcha" data-sitekey="<?=$this->pocore()->call->posetting[21]['value'];?>"></div>
+                                </div>
+                                <div class="col-sm-6 form-group">
+                                    <button type="submit" class="btn btn-primary btn-lg" required="required"><?=$this->e($comment_submit);?></button>
+                                </div>        
                             </div>
                         </form>     
                     </div><!--/#contact-page-->
-                    <?php } ?>
+                <?php } ?>
             </div>
             <!-- ./end of col-md-6 (main) -->
             
