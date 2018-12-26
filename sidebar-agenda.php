7bade1b11b794d366f38ae922e76999d51786692
pricelangela <45728920+pricelangela@users.noreply.github.com>
2018-12-27 06:36:08 +0800

    Update sidebar-agenda.php

--- sidebar-agenda.php
+++ sidebar-agenda.php
@@ -1,64 +1,44 @@
 <!-- Fluid width widget -->        
 <div class="panel panel-danger">
     <div class="panel-heading">
-        <h3 class="panel-title">
+        <h5 class="panel-title">
             <span class="glyphicon glyphicon-calendar"></span> 
             Calendar Events
-        </h3>
+        </h5>
     </div>
-    <div class="agenda owl-carousel owl-theme">
-        <div class="single-slide">
-            <div class="fadeInRight media">
+    <div class="panel-body">
+        <?php
+         $events = $this->pocore()->call->podb->from('event')
+            ->orderBy('id_event')
+            ->fetchAll();
+            foreach ($events as $event){
+        ?>
+        <ul class="media-list">
+            <li class="media">
                 <div class="media-left">
                     <div class="panel panel-danger text-center date">
                         <div class="panel-heading month">
-                            <span class="panel-title strong">Mar</span>
+                            <span class="panel-title strong">
+                                Tanggal Event
+                            </span>
                         </div>
-                        <div class="panel-body day text-danger">23
-                        </div>
-                    </div>
-                </div>
-                <div class="media-body">
-                    <h4 class="media-heading">Pulvinar Mauris Eu</h4>
-                    <p>Vivamus pulvinar mauris eu placerat blandit. In euismod tellus vel ex vestibulum congue.</p>
-                </div>
-            </div>
-        </div>
-        <div class="single-slide">
-            <div class="fadeInRight media">
-                <div class="media-left">
-                    <div class="panel panel-danger text-center date">
-                        <div class="panel-heading month">
-                            <span class="panel-title strong">Mar</span>
-                        </div>
-                        <div class="panel-body day text-danger">24
+                        <div class="panel-body day text-danger">
+                            <h4><?=$event['date_mulai'];?></h4>
                         </div>
                     </div>
                 </div>
-                <div class="media-body">
-                    <h4 class="media-heading">Pulvinar Mauris Eu</h4>
-                    <p>Vivamus pulvinar mauris eu placerat blandit. In euismod tellus vel ex vestibulum congue.</p>
+                <div class="media-body panel-padding">
+                    <h4 class="media-heading " >
+                      <?=$event['nama_event'];?>
+                    </h4>
+                    <p>
+                        <?=$event['desc_event'];?>
+                    </p>
                 </div>
-            </div>
-        </div>
-        <div class="single-slide">
-            <div class="fadeInRight media">
-                <div class="media-left">
-                    <div class="panel panel-danger text-center date">
-                        <div class="panel-heading month">
-                            <span class="panel-title strong">Mar</span>
-                        </div>
-                        <div class="panel-body day text-danger">25
-                        </div>
-                    </div>
-                </div>
-                <div class="media-body">
-                    <h4 class="media-heading">Pulvinar Mauris Eu</h4>
-                    <p>Vivamus pulvinar mauris eu placerat blandit. In euismod tellus vel ex vestibulum congue.</p>
-                </div>
-            </div>
-        </div>
-        </div>
+            </li>
+        </ul>
+    <?php } ?>
+        <a href="#" class="btn btn-default btn-block">More Events »</a>
+    </div>
 </div>
 <!-- End fluid width widget --> 
-
