924b7b5154e582bd5e57d3dff4efc207d405e7ee
BillyOroh <45728852+BillyOroh@users.noreply.github.com>
2018-12-31 05:47:21 +0800

    Update berita.php

--- berita.php
+++ berita.php
@@ -1,8 +1,24 @@
 <?=$this->layout('index');?>
-<div class="container-fluid berita-title">
-    <div class="page-title">
-        <h1>BERITA & INFO</h1>
+
+<style>
+    .breadcrumb {
+        background: none;
+    }
+
+</style>    
+
+<section class="header-selayang" id="header-visi-dan-misi">
+   <div class="jumbotron" style="background-image: url(<?=$this->asset('/images/gallery/gallery7.jpg')?>)">
+    <div class="container wow fadeIn">
+     <h1>Berita</h1>
+     <ol class="breadcrumb">
+        <li><a href="<?=BASE_URL;?>"><?=$this->e($front_home);?></a></li>
+        <li class="active"><?=$this->e($page_title);?></li>
+    </ol>
     </div>
+   </div>
+ </section>
+
 <div>
     
 <section id="berita">
